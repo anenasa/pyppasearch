3034adde9f3aff059dd1ed874f76a6f1ed34783e
N0rbert <309456+N0rbert@users.noreply.github.com>
2021-12-11 12:46:48 +0300

    Use launchpadlib instead of HTML parsing (#2)

--- README.md
+++ README.md
@@ -1,16 +1,22 @@
 # pyppasearch
 Simple python script for searching PPA
 
+To use it one should install Python 3 packages using `sudo apt-get install python3-requests python3-bs4 python3-launchpadlib`.
+
 ## Description
-This script will search from https://launchpad.net/ubuntu/+ppas, and then get packages information from each PPA, including package name, version and Ubuntu codename.
+This script will search from https://launchpad.net/ubuntu/+ppas, and then get packages information from each PPA, including package name, version and Ubuntu codename and CPU architecture.
 
 ## Usage
-    usage: pyppasearch.py [-h] [-c CODENAME] package
-    
-    positional arguments:
-      package               exact name of the package you want to search
-    
-    optional arguments:
-      -h, --help            show this help message and exit
-      -c CODENAME, --codename CODENAME
-                            First word of Ubuntu code name, e.g. Focal     
+```
+usage: pyppasearch.py [-h] [-c CODENAME] [-a ARCH] package
+
+positional arguments:
+  package               exact name of the package you want to search
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -c CODENAME, --codename CODENAME
+                        First word of Ubuntu code name, e.g. Focal
+  -a ARCH, --arch ARCH  CPU architecture, one of amd64, i386, armhf, arm64,
+                        etc.
+```
--- pyppasearch.py
+++ pyppasearch.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
+import argparse
 import requests
 from bs4 import BeautifulSoup
-import argparse
-
+from launchpadlib.launchpad import Launchpad
 
 class Repo:
     def __init__(self, user, name, url):
@@ -11,35 +11,30 @@ class Repo:
         self.name = name
         self.url = url
 
-    def search(self, search):
-        resp = requests.get(self.url)
-        soup = BeautifulSoup(resp.text, "html.parser")
-        thead = soup.select("thead > tr > th")
-        series_index = 3
-        # Sometimes there is "Uploader" in table, so position of index will change
-        for j in range(len(thead)):
-            if thead[j].text == "Series":
-                series_index = j
-                break
-        results = soup.select("tbody > tr.archive_package_row")
+    def search(self, lp, codename, cpu_arch, search):
+        owner = lp.people[self.user]
+        archive = owner.getPPAByName(distribution=lp.distributions["ubuntu"], name=self.name)
+        binaries = archive.getPublishedBinaries(status='Published', binary_name=search, exact_match=True)
+
         packages = []
-        for result in results:
-            source = result.select_one("a.sprite").text
-            name = source.split()[0]
-            version = source.split()[2]
-            series = result.select_one(f"td:nth-of-type({series_index + 2})").text
-            if name == search:
-                packages.append(Package(self.user, self.name, name, version, series))
+        if len(binaries) > 0:
+            for binary in binaries:
+                distro_arch_series = binary.distro_arch_series_link
+                binary_codename = distro_arch_series.split('/')[-2]
+                binary_arch = distro_arch_series.split('/')[-1]
+                if (codename is None or codename.lower() == binary_codename) and (cpu_arch is None or cpu_arch == binary_arch):
+                    packages.append(Package(self.user, self.name, binary.binary_package_name, binary.binary_package_version, binary_codename, binary_arch))
         return packages
 
 
 class Package:
-    def __init__(self, user, repo, name, version, series):
+    def __init__(self, user, repo, name, version, series, arch):
         self.user = user
         self.repo = repo
         self.name = name
         self.version = version
         self.series = series
+        self.arch = arch
 
 
 def search_ppa(search):
@@ -58,17 +53,18 @@ def search_ppa(search):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--codename", type=str, help="First word of Ubuntu code name, e.g. Focal")
+    parser.add_argument("-a", "--arch", type=str, help="CPU architecture, one of amd64, i386, armhf, arm64, etc.")
     parser.add_argument("package", type=str, help="exact name of the package you want to search")
     args = parser.parse_args()
+
+    lp = Launchpad.login_anonymously("ppa-search", "edge", "~/.launchpadlib/cache/", version="devel")
 
     repos = search_ppa(args.package)
     for i in range(len(repos)):
         print(f"\rSearching {i + 1}/{len(repos)}", end='')
-
-        packages = repos[i].search(args.package)
+        packages = repos[i].search(lp, args.codename, args.arch, args.package)
         for package in packages:
-            if args.codename is None or package.series == args.codename:
-                print(f"\r{package.name} {package.version} {package.user}/{package.repo} {package.series}")
+            print(f"\r{package.name} {package.version} ppa:{package.user}/{package.repo} {package.series.capitalize()} ({package.arch})")
 
     print("\rSearch is finished.")
 
